688e875e7c549d8b7c81970cef0c36e19ac85559
ibrahimatmaca <ibrahim.atmaca@bil.omu.edu.tr>
2025-12-25 15:08:56 +0300

    fix proıject horizontal align

--- components/Projects.tsx
+++ components/Projects.tsx
@@ -232,7 +232,7 @@ const Projects: React.FC = () => {
           </p>
         </div>
         
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 max-w-6xl mx-auto">
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
           {projects.map((p, idx) => (
             <ProjectCard key={p.id} project={p} index={idx} />
           ))}
